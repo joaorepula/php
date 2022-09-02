ce8612a2931a6fccc4c255b5b552d2ac6e0ae2a8
joaorepula <tahronjoao@gmail.com>
2022-09-02 10:47:03 -0300

    php iniciando tabela

---
+++ calculo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $b1 = floatval($_Post["nome"]);
+    $b2 = floatval($_Post["horas"]);
+    $b3 = floatval($_Post["valor"]);
+    $b4 = floatval($_Post["campo 3"]);   
+    $b5 = floatval($_Post["campo 4"]); ?>
+
+    <button onclick="history.back"></button>
+</body>
+</html>
---
+++ folha.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="calculo.php" method="post">
+    Nome:<input type=text name=nome required>
+    Horas Trabalhadas:<input type=text name=horas required>
+    Valor Hora:<input type=text name=valor required>
+    Calcular Sálario:<input type=submit value="enviar">
+	</form>
+
+    
+</body>
+</html>
